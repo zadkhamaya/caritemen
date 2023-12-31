eab5ff3e2ea827bad2e36379ac0ffeef7554245e
zdkh <azaryageoffrey@outlook.com>
2023-12-31 17:43:23 +0700

    feat: event-api

--- src/app/api/events/route.js
+++ src/app/api/events/route.js
@@ -1,10 +1,19 @@
 import { NextResponse } from "next/server";
 import prisma from "@/utils/prisma";
 import { uploadFile } from "@/lib/uploadFile";
+import slugify from "slugify";
 
 export async function GET() {
   try {
-    const allEvents = await prisma.event.findMany();
+    const allEvents = await prisma.event.findMany({
+      include: {
+        user: {
+          select: {
+            username: true,
+          },
+        },
+      },
+    });
     return NextResponse.json({
       data: allEvents,
       message: "All events fetched successfully",
@@ -23,31 +32,63 @@ export async function POST(request) {
   const description = formData.get("description");
   const featuredImage = formData.get("featuredImage");
   const images = formData.get("images");
-  const category = formData.get("category");
+  // const category = formData.get("category");
+  const userId = formData.get("userId");
+
+  let eventId = "";
+
+  try {
+    // const allImages = [];
+    // images.forEach((image) => {
+    //   allImages.push(image.name);
+    // });
+    const createEvent = await prisma.event.create({
+      data: {
+        title,
+        slug: slugify(title, { lower: true, replacement: "-" }),
+        date,
+        location,
+        description,
+        featuredImage: featuredImage.name,
+        // images: allImages,
+        user: {
+          connect: { id: userId },
+        },
+      },
+    });
+
+    eventId = createEvent.id;
+    console.log(createEvent);
+  } catch (error) {
+    console.log(error);
+  }
 
   // Send Images to AWS S3
   try {
+    // upload featuredImage
     const uploadFeaturedImage = await uploadFile({
       Body: featuredImage,
       Key: featuredImage.name,
       ContentType: featuredImage.type,
-      Dir: "events",
+      Dir: `events/${eventId}`,
     });
     console.log(uploadFeaturedImage);
+
+    // upload images file
+    images.forEach(async (item) => {
+      const uploadFeaturedImage = await uploadFile({
+        Body: item,
+        Key: item.name,
+        ContentType: item.type,
+        Dir: `events/${eventId}`,
+      });
+      console.log(uploadFeaturedImage);
+    });
   } catch (error) {
     console.log(error);
   }
 
   return NextResponse.json({
-    data: {
-      title,
-      date,
-      location,
-      description,
-      featuredImage,
-      images,
-      category,
-    },
     message: "Event created successfully",
   });
 }
