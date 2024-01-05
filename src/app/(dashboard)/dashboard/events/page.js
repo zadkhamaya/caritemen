149f61f7c332d334023bb6d43d8e46a4f3467d90
ZDKH <geoffrey.azarya@gmail.com>
2024-01-05 15:59:43 +0700

    fix: cache no-store error

--- src/app/(dashboard)/dashboard/events/page.js
+++ src/app/(dashboard)/dashboard/events/page.js
@@ -1,13 +1,14 @@
 import { Events } from "@/components/dashboard/components/events";
 import { AllEvent } from "@/components/events/allEvent";
-import { EventCard } from "@/components/events/eventCard";
 import { apiUrl, imageUrl } from "@/config/apiUrl";
-import Image from "next/image";
-import Link from "next/link";
+
 import React from "react";
 
 async function getData() {
-  const res = await fetch(`${apiUrl}/events`);
+  const res = await fetch(`${apiUrl}/events`, {
+    method: "GET",
+    cache: "no-store",
+  });
   const data = await res.json();
   return data;
 }
