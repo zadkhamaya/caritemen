3e0feb9a9c1dc3462ccf25534fb70cd5153c2255
ZDKH <geoffrey.azarya@gmail.com>
2024-01-06 10:08:45 +0700

    feat: api participants

--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -1,3 +1,4 @@
-export const apiUrl = "https://caritemen.vercel.app/api";
+// export const apiUrl = "https://caritemen.vercel.app/api";
+export const apiUrl = "https://localhost:3000/api";
 export const imageUrl =
   "https://caritemen-devscale.s3.ap-southeast-1.amazonaws.com/events";
