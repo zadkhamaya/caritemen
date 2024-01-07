170d45e8413775416268704a5c42accf95e672d0
Guntur Oktavianto Nugroho <gunturoktaviant@gmail.com>
2024-01-07 18:58:31 +0700

    feat: auth UI

--- src/components/auth/components/Logout.jsx
+++ src/components/auth/components/Logout.jsx
@@ -6,22 +6,22 @@ import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 
 export const Logout = () => {
-  const router = useRouter();
+    const router = useRouter();
 
-  const deleteCookie = (name) => {
-    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
-  };
+    const deleteCookie = (name) => {
+        document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+    };
 
-  const logoutHandler = () => {
-    deleteCookie("token");
-    toast.success("Logout Successfully"),
-      setTimeout(() => router.push("/login"), 500);
-  };
+    const logoutHandler = () => {
+        deleteCookie("token");
+        toast.success("Logout Successfully"),
+            setTimeout(() => router.push("/login"), 500);
+    };
 
-  return (
-    <div onClick={logoutHandler} className="menu">
-      <LogOut />
-      Logout
-    </div>
-  );
+    return (
+        <div onClick={logoutHandler} className="menu">
+            <LogOut />
+            Logout
+        </div>
+    );
 };
